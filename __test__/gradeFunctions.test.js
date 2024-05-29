360f4aae7fc23e810674e306cee4342cba7de48e
Mbithe <mbithemwangi@gmail.com>
2024-05-29 16:45:54 -0700

    few changes

--- __test__/gradeFunctions.test.js
+++ __test__/gradeFunctions.test.js
@@ -5,24 +5,24 @@ test('gradeMathPhysicsChemistry function', () => {
     expect(gradeMathPhysicsChemistry(75)).toBe("A");
     expect(gradeMathPhysicsChemistry(65)).toBe("B");
     expect(gradeMathPhysicsChemistry(55)).toBe("C");
-    expect(gradeMathPhysicsChemistry(45)).toBe("D");
-    expect(gradeMathPhysicsChemistry(35)).toBe("E");
+    expect(gradeMathPhysicsChemistry(45)).toBe("Pass");
+    expect(gradeMathPhysicsChemistry(35)).toBe("Supp");
 });
 
 test('gradeReligiousEducation function', () => {
     expect(gradeReligiousEducation(85)).toBe("A");
     expect(gradeReligiousEducation(75)).toBe("B");
     expect(gradeReligiousEducation(65)).toBe("C");
-    expect(gradeReligiousEducation(55)).toBe("D");
-    expect(gradeReligiousEducation(45)).toBe("E");
+    expect(gradeReligiousEducation(55)).toBe("Pass");
+    expect(gradeReligiousEducation(45)).toBe("Supp");
 });
 
 test('gradeEnglishSwahiliComputer function', () => {
     expect(gradeEnglishSwahiliComputer(80)).toBe("A");
     expect(gradeEnglishSwahiliComputer(65)).toBe("B");
     expect(gradeEnglishSwahiliComputer(55)).toBe("C");
-    expect(gradeEnglishSwahiliComputer(45)).toBe("D");
-    expect(gradeEnglishSwahiliComputer(35)).toBe("E");
+    expect(gradeEnglishSwahiliComputer(45)).toBe("Pass");
+    expect(gradeEnglishSwahiliComputer(35)).toBe("Supp");
 });
 
 test('calculateGradesAndAverage function', () => {
--- gradeFunctions.js
+++ gradeFunctions.js
@@ -2,24 +2,24 @@ const gradeMathPhysicsChemistry = (score) => {
   if (score >= 70) return "A";
   if (score >= 60) return "B";
   if (score >= 50) return "C";
-  if (score >= 40) return "D";
-  return "E";
+  if (score >= 40) return "Pass";
+  return "Supp";
 };
 
 const gradeReligiousEducation = (score) => {
   if (score >= 80) return "A";
   if (score >= 70) return "B";
   if (score >= 60) return "C";
-  if (score >= 50) return "D";
-  return "E";
+  if (score >= 50) return "Pass";
+  return "Supp";
 };
 
 const gradeEnglishSwahiliComputer = (score) => {
   if (score >= 75) return "A";
   if (score >= 60) return "B";
   if (score >= 50) return "C";
-  if (score >= 40) return "D";
-  return "E";
+  if (score >= 40) return "Pass";
+  return "Supp";
 };
 
 const calculateGradesAndAverage = (student) => {
